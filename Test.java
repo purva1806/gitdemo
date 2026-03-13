20a111efed11bb6772eb4be674a87700cf06086e
purva1806 <purvashah186@gmail.com>
2026-03-13 13:19:12 +0530

    Add new paragraph

--- Test.java
+++ Test.java
@@ -1,9 +1,9 @@
-public class test
+public class Test
 {
-       public static void main(String[] args)
+    public static void main(String[] args)
     {
         System.out.println("HEllo world");
     }
         
     
-}                                                                                                                                                                                                                                                                                                                                                                                                                                          
+}
